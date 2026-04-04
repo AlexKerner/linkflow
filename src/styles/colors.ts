b4aa35e0f7cb53b3aeff04f2a1ec8162d5c13f3d
Alexander <alexanderkerner06@gmail.com>
2026-04-04 10:58:45 -0300

    feat: colors and folder structure

---
+++ src/styles/colors.ts
@@ -0,0 +1,22 @@
+
+export const colors = {
+  light: {
+    primaryBlue: "#0F62FE",
+    bgPrimary: "#F8F9FF",
+    bgSecundary: "#EFF4FF",
+    bgTertiary: "#DDE1F9",
+    fontBold: "#05345C",
+    fontMedium: "#3D618C"
+  },
+  dark: {
+    primaryBlue: "#4D8EFF",
+    bgPrimary: "#00285D",
+    bgNav: "#020617",
+    bgCardPrimary: "#131B2E",
+    bgCardSecundary: "#222A3D",
+    fontBold: "#DAE2FD",
+    fontMedium: "#8C909F",
+    fontLight: "#C2C6D6"
+  }
+
+}
